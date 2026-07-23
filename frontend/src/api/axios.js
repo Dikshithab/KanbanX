bd3349e21e78bb022ac9f0b0ed0d5b20ac1218b9
Dikshithab <shivadikshithaburra0@gmail.com>
2026-07-23 21:43:45 +0530

    temp rename

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "https://kanbanx-backend-w554.onrender.com/api",});
+    baseURL: import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
