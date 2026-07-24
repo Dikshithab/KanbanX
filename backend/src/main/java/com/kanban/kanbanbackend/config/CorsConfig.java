dd66aa74c6aa2dbe9d468f77bd9ba199fa9e410b
Dikshithab <shivadikshithaburra0@gmail.com>
2026-07-24 08:32:17 +0530

    Fix CORS for Render frontend

--- backend/src/main/java/com/kanban/kanbanbackend/config/CorsConfig.java
+++ backend/src/main/java/com/kanban/kanbanbackend/config/CorsConfig.java
@@ -16,10 +16,21 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(    "http://localhost:5173"
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://kanbanx-frontend-mobk.onrender.com"
         ));
-        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
         configuration.setAllowedHeaders(List.of("*"));
+
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
